bc4fa35d6cd22c89ed16f9c57060dc00236b75f2
Lee <lee.jensen@agilix.com>
2022-11-09 15:53:35 -0700

    fix() deployment script

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+mkdir -p ../webprogramming260.github.io/simon-html
+cp *.{html} ../webprogramming260.github.io/simon-html
+cd ../webprogramming260.github.io
+git commit -am "updating simon-html project"
+git push
