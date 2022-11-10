4b27f93f194aab559364c5d92039f74d85b2bafb
Lee <lee.jensen@agilix.com>
2022-11-10 09:18:18 -0700

    fix(scores) match changes to later projects

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
+rm -r ../webprogramming260.github.io/simon-html
 mkdir -p ../webprogramming260.github.io/simon-html
 cp *.{html} ../webprogramming260.github.io/simon-html
 cd ../webprogramming260.github.io
-git commit -am "updating simon-html project"
+git commit -am "deploying simon-html project"
 git push
