e4dc5e9801b5cae2607498030abd5a6cd58d3f6e
Lee <lee.jensen@agilix.com>
2022-11-10 09:24:01 -0700

    fix(deploy) improved script to remove anything deleted

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
-rm -r ../webprogramming260.github.io/simon-html
+rm -rf ../webprogramming260.github.io/simon-html
 mkdir -p ../webprogramming260.github.io/simon-html
-cp *.{html} ../webprogramming260.github.io/simon-html
+cp *.html ../webprogramming260.github.io/simon-html
 cd ../webprogramming260.github.io
+git add .
 git commit -am "deploying simon-html project"
 git push
